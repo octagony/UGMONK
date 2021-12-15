1f3a1d8b0e8530c95c0296f32385c7fc19acf19f
Octagony <moctagony@gmail.com>
2021-12-15 21:57:33 +0300

    rewrite shop-btn and menu-btn on vanilla js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-jQuery(document).ready(function () {
-  $(".menu__btn").on("click", function () {
-    $(".menu__list").toggleClass("menu__list--active");
+const main = () => {
+  const menuBtn = document.querySelector(".menu__btn");
+  const menuList = document.querySelector(".menu__list");
+  const shopBtn = document.querySelector(".shop__btn");
+  const shopCarouselList = document.querySelector(".shop__carousel__list");
+
+  menuBtn.addEventListener("click", () => {
+    menuList.classList.toggle("menu__list--active");
   });
 
-  $(".shop__btn").on("click", function () {
-    $(".shop__carousel__list").toggleClass("shop__carousel__list--active");
+  shopBtn.addEventListener("click", () => {
+    shopCarouselList.classList.toggle("shop__carousel__list--active");
   });
 
-  jQuery("a.scrollto").click(function () {
-    elementClick = jQuery(this).attr("href");
-    destination = jQuery(elementClick).offset().top - 50;
-    jQuery("html:not(:animated),body:not(:animated)").animate(
-      { scrollTop: destination },
-      1000
-    );
-    return false;
-  });
+  jQuery(document).ready(function () {
+    jQuery("a.scrollto").click(function () {
+      elementClick = jQuery(this).attr("href");
+      destination = jQuery(elementClick).offset().top - 50;
+      jQuery("html:not(:animated),body:not(:animated)").animate(
+        { scrollTop: destination },
+        1000
+      );
+      return false;
+    });
 
-  $(function () {
-    var wi = $(document).width();
-    if (wi > 1200) {
-      $(window).scroll(function () {
-        if ($(this).scrollTop() >= 187) {
-          $(".header__title").addClass("sticky slide-in-top");
-        } else {
-          $(".header__title").removeClass("sticky slide-in-top");
-        }
-      });
-    }
+    $(function () {
+      var wi = $(document).width();
+      if (wi > 1200) {
+        $(window).scroll(function () {
+          if ($(this).scrollTop() >= 187) {
+            $(".header__title").addClass("sticky slide-in-top");
+          } else {
+            $(".header__title").removeClass("sticky slide-in-top");
+          }
+        });
+      }
+    });
   });
-});
+};
+
+main();
